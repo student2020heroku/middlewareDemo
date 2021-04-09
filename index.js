a69a1e25c085d03a2505566d4e0344918f231840
Kirillakrill <akrillsm9003@gmail.com>
2021-04-09 18:10:10 +0300

    add devDependencies and index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const [{ Server: h1 }, x] = [require('http'), require('express')];
+
+const Router = x.Router();
+const PORT = 4321;
+const { log } = console;
+const hu = { 'Content-Type': 'text/html; charset=utf-8' };
+const app = x();
+Router
+  .route('/')
+  .get(r => r.res.end('Привет мир!'));
+app
+  .use((r, rs, n) => rs.status(200).set(hu) && n())
+  .use(x.static('.'))
+  .use('/', Router)
+  .use(({ res: r }) => r.status(404).set(hu).send('Пока нет!'))
+  .use((e, r, rs, n) => rs.status(500).set(hu).send(`Ошибка: ${e}`))
+  /* .set('view engine', 'pug') */
+  .set('x-powered-by', false);
+module.exports = h1(app)
+  .listen(process.env.PORT || PORT, () => log(process.pid));
+
+
+/*
+
+для варианта с type="module"
+
+import { Server } from 'http';
+import x from 'express';
+
+и в предпоследней строке
+
+export default Server(app)
+
+*/
