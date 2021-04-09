82d63ce7e8ac80188006278f5acb7a9d45464136
Kirillakrill <60612332+Kirillakrill@users.noreply.github.com>
2021-04-09 21:15:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,27 @@ const PORT = 4321;
 const { log } = console;
 const hu = { 'Content-Type': 'text/html; charset=utf-8' };
 const app = x();
+const mw0 = (r, rs, n) => rs.status(200).set(hu) && n();
 Router
   .route('/')
   .get(r => r.res.end('Привет мир!'));
 app
-  .use((r, rs, n) => rs.status(200).set(hu) && n())
+  .use(mw0)
+  .use(function workingSetter(req, res, next) {req.working = 'Работает, ура!'; next();})
   .use(x.static('.'))
   .use('/', Router)
-  .use(({ res: r }) => r.status(404).set(hu).send('Пока нет!'))
+  .get('/first', (req, res, next) => {
+    req.app._router.stack.forEach(mw => console.log(mw.name))
+    if (req.query.error == 'yes') return next();   
+    res.send(req.working);
+  })
+  .use((req, res, next) => { req.errorMessage = 'Всё ещё нет'; next(); })
+  .use(r => r.res.status(404).set(hu).send(r.errorMessage))
   .use((e, r, rs, n) => rs.status(500).set(hu).send(`Ошибка: ${e}`))
   /* .set('view engine', 'pug') */
   .set('x-powered-by', false);
 module.exports = h1(app)
   .listen(process.env.PORT || PORT, () => log(process.pid));
-
 
 /*
 
