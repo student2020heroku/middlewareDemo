619b3868ef7675199fddeeba6589f6bb87bcd70b
Kirillakrill <akrillsm9003@gmail.com>
2021-04-11 16:36:10 +0300

    add rout '/mpy/:n1/:n2'

--- api/index.js
+++ api/index.js
@@ -6,7 +6,13 @@ module.exports = x => {
 
     router
     .route('/add/:n1/:n2')
-    .get(r => r.res.send('Got:' + Number(r.params.n1) + Number(r.params.n2)));
+    .get(r => r.res.send('Got:' + Number(r.params.n1) + Number(r.params.n2)))
+    .post(r => r.res.send('Posted:' + Number(r.params.n1) + Number(r.params.n2)));
+
+    router
+    .route('/mpy/:n1/:n2')
+    .get(r => r.res.send('Got:' + Number(r.params.n1 * r.params.n2)))
+    .post(r => r.res.send('Posted:' + Number(r.params.n1 * r.params.n2)));
 
     return router
 };
