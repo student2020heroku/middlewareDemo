4b9e682177f2f76816a6e3d20456b42ca86c557e
Kirillakrill <akrillsm9003@gmail.com>
2021-04-11 16:47:07 +0300

    fix#4

--- routes/api2/index.js
+++ routes/api2/index.js
@@ -2,7 +2,7 @@ module.exports = x => {
     const router = x.Router();
     router
     .route('/')
-    .get(r => r.res('not yet implemented'))
+    .get(r => r.res.send('not yet implemented'))
 
     return router
 };
